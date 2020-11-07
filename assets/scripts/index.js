61a366fc673ca4d388011bc3275a0294c721e371
ducktrshessami <jwee761@gmail.com>
2020-11-07 14:52:35 -0800

    form submit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,18 @@
+var zipSearch = $("#search-zip-code");
 
+let lastSearch = localStorage.getItem("lastSearch");
+if (lastSearch) {
+    zipSearch.val(lastSearch);
+}
+
+$(document.body).ready(function() {
+    $("form").on("submit", function(event) {
+        let zip = zipSearch.val();
+
+        event.preventDefault();
+
+        zpGetCoords(zip).then(() => {
+            window.location = "./venue-list.html";
+        }).catch(console.error);
+    });
+})
