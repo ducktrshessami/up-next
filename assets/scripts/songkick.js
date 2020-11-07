47a9baecf7ebf617ebdbbc7ac2b1517df50ac0da
ducktrshessami <jwee761@gmail.com>
2020-11-07 11:06:15 -0800

    location search desc

--- assets/scripts/songkick.js
+++ assets/scripts/songkick.js
@@ -1,2 +1,15 @@
 const skapi1 = "pE1BwpmMDHJdfs9n"; // We have 2 API keys for no reason in particular
 const skapi2 = "4t1Ilns2EQEjn4QW";
+
+/*
+Songkick API call to search for location
+As of right now the query is a little redundant since ZIP code is required, but just roll with it
+
+@param zip: a string containing the ZIP code
+@param query: a string containing the city name to search for
+
+@return: a Promise<Array<object>> that resolves in a list of locations
+*/
+function skLocationSearch(zip, query) {
+    
+}
