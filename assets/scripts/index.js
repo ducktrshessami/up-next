64ca2d01b15c3fd43536fe1f1d1653fac8335d9e
ducktrshessami <jwee761@gmail.com>
2020-11-07 10:32:08 -0800

    moved api keys

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,2 +1,1 @@
-const skapi1 = "pE1BwpmMDHJdfs9n";
-const skapi2 = "4t1Ilns2EQEjn4QW";
+
---
+++ assets/scripts/songkick.js
@@ -0,0 +1,2 @@
+const skapi1 = "pE1BwpmMDHJdfs9n"; // We have 2 API keys for no reason in particular
+const skapi2 = "4t1Ilns2EQEjn4QW";
