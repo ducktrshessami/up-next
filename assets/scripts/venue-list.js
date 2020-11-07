0267c76756ae728e2e7543f240b51a21818ab3fe
NDPenaloza <ndpenaloza@gmail.com>
2020-11-07 13:41:42 -0800

    created variable searchCity to grab search-city-input and added to function to locate the searched city

--- assets/scripts/venue-list.js
+++ assets/scripts/venue-list.js
@@ -1,15 +1,20 @@
 // Initialize and add the map
 function initMap() {
+    
+    // city search input
+    var searchCity = $("#search-city");
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
+    //const uluru = { lat: -25.344, lng: 131.036 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+      zoom: 15,
+      center: searchCity,
+      //center: uluru,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      //position: uluru,
+      position: searchCity,
       map: map,
     });
   }
