ffd0630bffd599033e48b4b6e9ad60171c9aca89
ducktrshessami <jwee761@gmail.com>
2020-11-07 14:04:35 -0800

    restructuring

--- assets/scripts/songkick.js
+++ assets/scripts/songkick.js
@@ -4,6 +4,31 @@ const skapi2 = "4t1Ilns2EQEjn4QW";
 const perPage = 50;
 
 /*
+Zippopotam.us API call to search for lat/lon coordinates
+
+@param zip: a string containing the ZIP code
+
+@return: a Promise<object> that resolves in {
+    lat: float
+    lon: float
+}
+*/
+function zpGetCoords(zip) {
+    return new Promise(function(resolve, reject) {
+        $.ajax({
+            method: "GET",
+            url: "http://api.zippopotam.us/us/" + zip,
+            error: reject
+        }).then((response) => {
+            resolve({
+                lat: parseFloat(response.places[0].latitude),
+                lon: parseFloat(response.places[0].longitude)
+            });
+        });
+    });
+}
+
+/*
 Songkick API call to search for events from a location
 As of right now the query is a little redundant since ZIP code is required, but just roll with it
 
@@ -12,22 +37,20 @@ As of right now the query is a little redundant since ZIP code is required, but 
 
 @return: a Promise<Array<object>> that resolves in a list of events
 */
-function skGetEventList(zip, query) {
-    return new Promise(function(resolve, reject) {
-        zpGetState(zip).then(async (coords) => {
-            let maxPages = 1;
-            let results = [];
-            let queryURL = `https://api.songkick.com/api/3.0/events.json?apikey=${skapi1}&location=geo:${coords.lat},${coords.lon}&per_page=${perPage}`;
-            for (let i = 1; i <= maxPages; i++) {
-                let currentPage = await $.ajax({
-                    method: "GET",
-                    url: `${queryURL}&page=${i}`
-                });
-                maxPages = Math.ceil(currentPage.resultsPage.totalEntries / perPage); // I couldn't think of a way to only do this once and not defeat the purpose
-                results = results.concat(currentPage.resultsPage.results.event);
-            }
-            resolve(results);
-        })
+function skGetEventList(coords) {
+    return new Promise(async function(resolve, reject) {
+        let maxPages = 1;
+        let results = [];
+        let queryURL = `https://api.songkick.com/api/3.0/events.json?apikey=${skapi1}&location=geo:${coords.lat},${coords.lon}&per_page=${perPage}`;
+        for (let i = 1; i <= maxPages; i++) {
+            let currentPage = await $.ajax({
+                method: "GET",
+                url: `${queryURL}&page=${i}`
+            });
+            maxPages = Math.ceil(currentPage.resultsPage.totalEntries / perPage); // I couldn't think of a way to only do this once and not defeat the purpose
+            results = results.concat(currentPage.resultsPage.results.event);
+        }
+        resolve(results);
     });
 }
 
@@ -54,29 +77,4 @@ function skGetVenueList(eventList) {
         }
     }
     return venues;
-}
-
-/*
-Zippopotam.us API call to search for lat/lon coordinates
-
-@param zip: a string containing the ZIP code
-
-@return: a Promise<object> that resolves in {
-    lat: float
-    lon: float
-}
-*/
-function zpGetState(zip) {
-    return new Promise(function(resolve, reject) {
-        $.ajax({
-            method: "GET",
-            url: "http://api.zippopotam.us/us/" + zip,
-            error: reject
-        }).then((response) => {
-            resolve({
-                lat: parseFloat(response.places[0].latitude),
-                lon: parseFloat(response.places[0].longitude)
-            });
-        });
-    });
 }
