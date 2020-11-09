c5ea0d66c96582d90407ce74cf99cfa44064eb27
NDPenaloza <ndpenaloza@gmail.com>
2020-11-08 22:50:10 -0800

    changed variables searchCity to searchZipCode

--- assets/scripts/venue-list.js
+++ assets/scripts/venue-list.js
@@ -41,19 +41,19 @@ function displayVenueList(venueList) {
 // Initialize and add the map
 function initMap() {    
     // city search input
-    var searchCity = $("#search-city").val().trim();
+    var searchZipCode = $("#search-zip-code").val().trim();
     // The location of Uluru
     //const uluru = { lat: -25.344, lng: 131.036 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
-        center: searchCity,
+        center: searchZipCode,
         //center: uluru,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
         //position: uluru,
-        position: searchCity,
+        position: searchZipCode,
         map: map,
     });
 }
