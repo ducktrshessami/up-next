2c8d17990114a9640f4d36f9eade1dccf9ef4d42
ducktrshessami <jwee761@gmail.com>
2020-11-09 20:09:17 -0800

    event filtering

--- assets/scripts/songkick.js
+++ assets/scripts/songkick.js
@@ -31,6 +31,7 @@ function zpGetCoords(zip) {
 /*
 Songkick API call to search for events from a location
 As of right now the query is a little redundant since ZIP code is required, but just roll with it
+Only events that have a venue and are of status "ok" are returned
 
 @param zip: a string containing the ZIP code
 @param query: a string containing the city name to search for
@@ -48,7 +49,7 @@ function skGetEventList(coords) {
                 url: `${queryURL}&page=${i}`
             });
             maxPages = Math.ceil(currentPage.resultsPage.totalEntries / perPage); // I couldn't think of a way to only do this once and not defeat the purpose
-            results = results.concat(currentPage.resultsPage.results.event);
+            results = results.concat(currentPage.resultsPage.results.event.filter(e => e.status == "ok" && e.venue.id));
         }
         resolve(results);
     });
@@ -64,17 +65,15 @@ Parse an event list into a venue list (with events in each venue)
 function skGetVenueList(eventList) {
     let venues = [];
     for (let i = 0; i < eventList.length; i++) {
-        if (eventList[i].venue.id) { // Only parse events that have venues
-            let vi = venues.findIndex(v => v.id == eventList[i].venue.id);
-            if (vi === -1) { // If venue not in list
-                vi = venues.length;
-                venues.push(eventList[i].venue);
-            }
-            if (!venues[vi].events) { // If new venue in list
-                venues[vi].events = [];
-            }
-            venues[vi].events.push(eventList[i]);
+        let vi = venues.findIndex(v => v.id == eventList[i].venue.id);
+        if (vi === -1) { // If venue not in list
+            vi = venues.length;
+            venues.push(eventList[i].venue);
         }
+        if (!venues[vi].events) { // If new venue in list
+            venues[vi].events = [];
+        }
+        venues[vi].events.push(eventList[i]);
     }
     return venues;
 }
