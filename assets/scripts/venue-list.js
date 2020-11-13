2ac91ccecb7026f86a05944a11930164a3a09cb2
ducktrshessami <jwee761@gmail.com>
2020-11-12 21:50:49 -0800

    removed page flow comment

--- assets/scripts/venue-list.js
+++ assets/scripts/venue-list.js
@@ -29,22 +29,6 @@ async function initPage() {
     venueListEl.click(gotoVenue);
     venuePaginationEl.click(handlePagination);
 }
-/*
-If my intuition is correct, we can remove &callback=initMap from the Google Maps
-script tag inside venue-list.html. Then we move the Google Maps script tag above
-this current script's tag as it was originally. Then this page's programming
-flows as follows:
-
-> HTML loads
-> Google Maps API loads (but doesn't call initMap)
-> venue-list.js begins
-> initPage gets called
-> displayVenueList gets called from within initPage
-> Venue list gets built and displayed
-> Map centered on stored coordinates gets created
-> Map markers for every venue in list gets created
-> Map gets displayed
-*/
 
 /*
 Handle query
