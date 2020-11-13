29defa9b88bb0242aeff072d26e8da9c9eadc2d2
ducktrshessami <jwee761@gmail.com>
2020-11-12 18:36:01 -0800

    venue details and begin event list

--- assets/scripts/event-list.js
+++ assets/scripts/event-list.js
@@ -1,6 +1,15 @@
 var venueID, pageNum;
 
 const venDetailsEl = $("#details");
+const venImgEl = $("#ven-image");
+const venNameEl = $("#ven-name");
+const venCapEl = $("#ven-cap");
+const venDescEl = $("#ven-desc");
+const venAddrEl = $("#ven-addr");
+const venPhoneEl = $("#ven-phone");
+const venWebEl = $("#ven-web");
+const paginEl = $("#event-pagination");
+const eventListEl = $("#event-list");
 
 initPage();
 
@@ -41,12 +50,35 @@ function handleArgs() {
 */
 async function displayVenueDetails(venueDetails) {
     console.log(venueDetails);
+    venImgEl.attr("src", `https://images.sk-static.com/images/media/profile_images/venues/${venueID}/col6`);
+    venImgEl.attr("alt", venueDetails.displayName);
+    venNameEl.text(venueDetails.displayName);
+    venCapEl.text(venueDetails.capacity);
+    venDescEl.text(venueDetails.description);
+    venAddrEl.text(venueDetails.street);
+    venPhoneEl.text(venueDetails.phone);
+    venWebEl.text(venueDetails.website);
+    venWebEl.attr("href", venueDetails.website);
 }
 
 /*
 */
 async function displayEventList(eventList) {
     console.log(eventList);
+    for (let i = 0; i < eventList.length; i++) {
+        eventListEl.append(`
+            <li class="col s12 m6 xl4">
+                <div class="card black white-text" role="button">
+                    <div class="card-content">
+                        <img src="https://images.sk-static.com/images/media/profile_images/artists/${eventList[i].performance[0].artist.id}/huge_avatar" alt="Artist_IMAGE" class="responsive-img circle right artist-image">
+                        <span class="card-title">Event/Artist Name</span>
+                        <p>Showtime 7:30 PM</p>
+                        <p>Short description of artist.</p>
+                    </div>
+                </div>
+            </li>
+        `);
+    }
 }
 
 /*
